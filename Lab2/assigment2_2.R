cb338b5dd4f185686bd831cb762b2e16e66ddad0
tiyaroy123 <85572348+tiyaroy123@users.noreply.github.com>
2021-08-26 23:46:00 +0530

    Update assigment2_2.R

--- Lab2/assigment2_2.R
+++ Lab2/assigment2_2.R
@@ -1,11 +1,21 @@
-#1
+#a
 getwd()
 setwd("C:/Users/suche/Downloads/M.Tech_3rd_Sem/Data Analytics Lab")
 data <- read.csv('iris.csv')
 View(data)
 
-#2
+#b
 mean(data$sepal_length)
 mean(data$sepal_width)
 mean(data$petal_length)
 mean(data$petal_width)
+
+#c
+agg_sum = aggregate(data[,1:4],by=list(data$class),FUN=sum, na.rm=TRUE)
+agg_sum
+
+#d
+agg_count = aggregate(data[,1:4],by=list(data$class),FUN=length)
+agg_count
+
+#e
