bfd17b56c9f19eea8edb06bee30631019968f4de
tiyaroy123 <85572348+tiyaroy123@users.noreply.github.com>
2021-09-16 22:30:41 +0530

    Update assigment5_2.R

--- Lab5/assigment5_2.R
+++ Lab5/assigment5_2.R
@@ -1,1 +1,16 @@
 #a
+pnorm(74, mean=70, sd=2, lower.tail=FALSE)
+
+#b
+qnorm(.99, mean=0, sd=1)
+
+#c
+
+P <- 1:10/sum(1:10)
+Q <- 20:29/sum(20:29)
+
+x <- rbind(P,Q)
+install.packages('philentropy')
+library(philentropy)
+
+distance(x, method = "euclidean")
