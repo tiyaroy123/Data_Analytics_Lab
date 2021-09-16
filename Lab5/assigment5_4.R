ee22e059bf24c44985e40acb5e4c3b13724e6391
tiyaroy123 <85572348+tiyaroy123@users.noreply.github.com>
2021-09-17 00:50:20 +0530

    Update assigment5_4.R

--- Lab5/assigment5_4.R
+++ Lab5/assigment5_4.R
@@ -4,3 +4,19 @@ getwd()
 setwd("C:/Users/suche/Downloads/M.Tech_3rd_Sem/Data Analytics Lab")
 data <- read.csv('iris.csv')
 View(data)
+
+#b
+
+plot(x=data$petal_length,y =data$petal_width, xlab = 'Petal_Length', ylab = 'Petal_Width')
+
+#c
+
+x = subset(data, species == 'Iris-setosa')[,1:4]
+y = subset(data, species == 'Iris-versicolor')[,1:4]
+z = subset(data, species == 'Iris-virginica')[,1:4]
+x1 <- rbind(x,y)
+y1 <- rbind(y,z)
+z1 <- rbind(x,z)
+stats::dist(x1, method = "euclidean") 
+stats::dist(y1, method = "euclidean") 
+stats::dist(z1, method = "euclidean") 
