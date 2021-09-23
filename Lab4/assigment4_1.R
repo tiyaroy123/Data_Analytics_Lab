55e191a22ad4b9826b83c44bce7dc063dbf72afc
tiyaroy123 <85572348+tiyaroy123@users.noreply.github.com>
2021-09-24 00:31:56 +0530

    Update assigment4_1.R

--- Lab4/assigment4_1.R
+++ Lab4/assigment4_1.R
@@ -1,1 +1,32 @@
+#a
+v <- c(1:10)
+p <- dbinom(v, 11, 0.7)
+barplot(p, v)
 
+#b
+c <- cumsum(table(v))
+barplot(c, v)
+
+#c
+a <- c(2,4,6)
+b <- dbinom(a, 10, 0.16)
+plot(b)
+
+#d
+c <- dbinom(a, 10, 0.05)
+plot(c)
+
+d <- dbinom(a, 10, 0.2)
+plot(d)
+
+e <- dbinom(a, 10, 0.5)
+plot(e)
+
+f <- dbinom(a, 10, 0.8)
+plot(f)
+
+#e
+
+x <- c(1:10)
+y <- dpois(x, 0.2)
+plot(y)
